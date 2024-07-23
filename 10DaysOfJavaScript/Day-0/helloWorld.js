b04f8dda63ff83f600e7fedcf25ede9c36db36df
Stheven <sthevenalvess@gmail.com>
2024-07-23 15:04:08 -0300

    day 0

---
+++ 10DaysOfJavaScript/Day-0/helloWorld.js
@@ -0,0 +1,13 @@
+
+function main() {
+    let parameterVariable = 'Hello My Friend';
+    
+    greeting(parameterVariable);
+}
+
+function greeting(parameterVariable) {
+    console.log('Hello, World!');
+    console.log(parameterVariable);  
+}
+
+main();
