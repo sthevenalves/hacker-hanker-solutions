c6bdf50c388d152708c64ca36cf555da743dab2c
Stheven <sthevenalvess@gmail.com>
2024-08-21 11:33:52 -0300

    new soluctions

--- Algorithms/Implementation/MigratoryBirds.js
+++ Algorithms/Implementation/MigratoryBirds.js
@@ -1,19 +1,25 @@
 function migratoryBirds(arr) {
-    // Write your code here
-    let count = {}; // {value: count}
-    let maxRep = arr[0], maxCount = arr[0];
-    for (let value of arr) {
-        if (count[value]) {
-            count[value] += 1;
+    let count = {}; //count {1: 3, 2: 5} - {value: rep}
+    let maxRep = arr[0], maxCount = 0;
+
+    for (let value of arr) { 
+        if (count[value]) { //existe no objeto count
+            count[value] += 1; // Incrementa o valor associado à chave
         } else {
-            count[value] = 1;
+            count[value] = 1; // Inicializa o valor associado à chave
         }
-        if (count[value] > maxCount) {
+
+        if (count[value] > maxCount || (count[value] === maxCount && value < maxRep)) {
             maxCount = count[value];
             maxRep = value;
         }
     }
+
     return maxRep;
 }
 
-console.log(migratoryBirds([1,1,1,2,2,2,4]));
+const arr0 = [1, 4, 4, 4, 5, 3];
+console.log(migratoryBirds(arr0)); 
+
+const arr1 = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4];
+console.log(migratoryBirds(arr1));  
